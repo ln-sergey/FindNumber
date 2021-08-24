a7d3b93a8afaac86b005bf03076e540b54f9b95e
ln_sergey <lnov.sergey@gmail.com>
2021-08-24 23:10:11 +0300

    * Add Navigation Controller * Add RecordViewController.swift * Add SettingsViewController.swift * Fix LaunchScreen.storyboard

---
+++ FindNumber/RecordViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecordViewController.swift
+//  FindNumber
+//
+//  Created by Sergey Lobanov on 24.08.2021.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ FindNumber/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  FindNumber
+//
+//  Created by Sergey Lobanov on 24.08.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
